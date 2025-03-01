362cde9682fee6aed495087b23fdbadff5e2fd31
caffeine0coffee <dev@caffeinezero.info>
2025-03-02 08:15:57 +0900

    feat: add state updator member function to Mouse

--- src/app/context/keyboard.hpp
+++ src/app/context/keyboard.hpp
@@ -4,6 +4,8 @@
 
 class Keyboard {
   public:
+    void UpdateFromKeyCallback(int key, int scanCode, int action, int mods);
+
     [[nodiscard]] bool ctrl() const { return ctrl_; }
     [[nodiscard]] bool alt() const { return alt_; }
     [[nodiscard]] bool shift() const { return shift_; }
